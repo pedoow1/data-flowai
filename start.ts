af9d82bfee2ccf4498e1ea8516d813af569dc649
pedoow1 <131590379+pedoow1@users.noreply.github.com>
2026-06-01 19:31:06 +0300

    Add files via upload

---
+++ start.ts
@@ -0,0 +1,5 @@
+import { startJobWorker } from "../server/workers/job-processor";
+
+if (process.env.NODE_ENV !== "development" || process.env.RUN_WORKER === "true") {
+  startJobWorker().catch(console.error);
+}
